f26f5d5b74cc6df19840ca3515607e6279e15f85
DestELYK <destelyk007@gmail.com>
2024-07-11 12:19:33 -0400

    Updated menu

--- install.sh
+++ install.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
+"""
+This script installs the required scripts for the MediaScreen system.
+
+The script downloads the configuration file and required scripts from the GitHub repository.
+The user can choose to run a specific script or run the full installation.
+
+This script requires root privileges. Please run as root.
+
+Command Usage:
+    - To run the full installation:
+        sudo bash install.sh --full-install
+    - To run the full installation and autolaunch with specific username:
+        sudo bash install.sh --full-install --username <username>
+    - To run a specific script:
+        sudo bash install.sh
+
+Author: DestELYK
+Date: 07-09-2024
+"""
+
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
-# Check if connected to the internet
-echo "Checking internet connection..."
-attempts=0
-while ! ping -q -c 1 -W 1 google.com >/dev/null; do
-    attempts=$((attempts+1))
-    if [ $attempts -gt 3 ]; then
-        echo "Failed to connect to the internet after 3 attempts. Exiting..."
-        exit 1
-    fi
-
-    echo "Not connected to the internet. Configuring WiFi..."
-    read -p "Enter the SSID: " SSID
-    read -p "Enter the password: " PASSWORD
-    # Connect to WiFi using wpa_supplicant
-    wpa_passphrase "$SSID" "$PASSWORD" | tee /etc/wpa_supplicant.conf > /dev/null
-    wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant.conf
-
-    # Refresh network interfaces
-    echo "Refreshing network interfaces..."
-    systemctl restart networking
-done
-echo "Connected to the internet."
+# Check internet connection
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+    echo "Internet connected"
+else
+    echo "Internet not connected. Exiting..."
+    exit 1
+fi
 
 # Base URL for scripts and configuration file
 base_url="https://raw.githubusercontent.com/DestELYK/mediascreen-installer/main"
@@ -37,29 +42,37 @@ tmp_dir=$(mktemp -d)
 cd "$tmp_dir"
 
 # Download the configuration file
-wget -q "${config_url}" -O menu_config.txt
+wget -q "${config_url}" -O menu_config.txt || {
+    echo "Failed to download configuration file. Exiting..."
+    exit 1
+}
 
 # Read the configuration file and generate menu options
 declare -a menu_names
 declare -a menu_descriptions
 declare -a script_filenames
+echo "Downloading required scripts..."
 while IFS=, read -r name description filename; do
     filename=$(echo $filename | tr -d '\r') # Remove carriage return
 
     menu_names+=("$name")
     menu_descriptions+=("$description")
     script_filenames+=("$filename")
-    echo "Downloading $name script as $filename..."
     # Download the script file
     wget -q "${base_url}/scripts/${filename}" -O "${filename}" || {
         echo "Failed to download script: ${filename}. Exiting..."
         exit 1
     }
+    chmod +x "$filename"
+    chown ${SUDO_USER}:${SUDO_USER} "$filename"
+    
     # Move the script file to /usr/local/bin
     mv "${filename}" "/usr/local/bin/${filename}"
 
-    chmod +x "/usr/local/bin/${filename}"
 done < menu_config.txt
+echo "Finished downloading required scripts"
+
+sleep 1
 
 full_install() {
     echo "Running full install..."
@@ -105,31 +118,62 @@ full_install() {
 
 # Function to display the menu
 show_menu() {
-    echo "Select an installation option:"
+    clear
+
+    echo "+-----------------------------------------------------------------------------------------+"
+    echo "|                                 MediaScreen Installer                               |"
+    echo "+-----------------------------------------------------------------------------------------+"
     echo "0) Full Install"
     for i in "${!menu_names[@]}"; do
             echo "$((i+1))) ${menu_names[$i]} - ${menu_descriptions[$i]}"
     done
-    echo "$((i+2))) Exit"
+    echo "==========================================================================================="
+    echo "                            u - Update | r - Reboot | q - Exit"
+    echo "==========================================================================================="
 }
 
 # Function to run a selected script
 run_option() {
-    if [ "$1" -eq 0 ]; then
-        full_install
-    elif [ "$1" -le "${#menu_names[@]}" ]; then
-        local script="${script_filenames[$(($1-1))]}"
-        bash $script
-        if [ $? -ne 0 ]; then
-                echo "Script failed: $script. Exiting..."
+    case $1 in
+        0)
+            full_install
+            ;;
+        [1-9]*)
+            local script="${script_filenames[$(($1-1))]}"
+            bash $script || {
+                echo "Failed to run script: $script. Exiting..."
                 exit 1
-        fi
-    elif [ "$1" -eq "$((${#menu_names[@]}+1))" ]; then
-        echo "Exiting..."
-        exit
-    else
-        echo "Invalid option. Please try again."
-    fi
+            }
+            ;;
+        u|update)
+            echo "Updating..."
+            wget -q "${base_url}/install.sh" -O install.sh || {
+                echo "Download failed. Please enter the URL:"
+                read -r new_url
+                wget -q "$new_url" -O install.sh || {
+                    echo "Download failed again. Please check the URL and try again later."
+                    return 1
+                }
+            }
+            chmod +x install.sh
+            mv install.sh /usr/local/bin/mediascreen-util.sh
+            echo "Updated successfully. This script will now exit."
+            exit
+            ;;
+        r|reboot)
+            echo "Rebooting in 10 seconds..."
+            sleep 10
+            reboot
+            ;;
+        q|quit|exit)
+            echo "Exiting..."
+            exit
+            ;;
+        *)
+            echo "Invalid option. Please try again."
+            return
+            ;;
+    esac
 }
 
 # Check for argument "--full-install"
