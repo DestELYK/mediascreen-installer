eabef8576fc0e535fae9839a2a35665a48df21e6
DestELYK <destelyk007@gmail.com>
2024-07-17 11:11:05 -0400

    Implemented displaying current rules and prompts at the start for adding rules

--- scripts/firewall-setup.sh
+++ scripts/firewall-setup.sh
@@ -29,24 +29,27 @@ apt install ufw -y
 echo "Configuring firewall rules..."
 ufw default deny incoming
 
+echo "Current rules: "
+ufw status numbered
+
 echo Available IP addresses:
 ip addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1'
 
 # Ask for IP subnet
 while true; do
+    read -p "Do you want to add more IP subnets? (y/n): " ADD_MORE
+    if [[ $ADD_MORE =~ ^[Yy]$ ]]; then
+        continue
+    else
+        break
+    fi
+
     read -p "Enter the IP subnet to allow (in the format '_._._._/__'): " IP_SUBNET
     # Validate IP subnet format
     if [[ ! $IP_SUBNET =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
         echo "Invalid IP subnet format. Please enter in the format '_._._._/_'."
     else
         ufw allow from $IP_SUBNET to any
-
-        read -p "Do you want to add more IP subnets? (y/n): " ADD_MORE
-        if [[ $ADD_MORE =~ ^[Yy]$ ]]; then
-            continue
-        else
-            break
-        fi
     fi
 done
 
